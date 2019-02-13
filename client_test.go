8b6d2225de4ae01a6592b2660b8f0f6c126b4ca3
epy0n0ff <epy0n.0ff@gmail.com>
2019-02-13 18:52:46 +0900

    Bump akamai ccu api version

--- client_test.go
+++ client_test.go
@@ -1,23 +1,36 @@
 package restclient
 
 import (
+	"bufio"
 	"context"
 	"os"
 	"testing"
+
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/edgegrid"
 )
 
-var user = os.Getenv("AKAMAI_USER")
-var pass = os.Getenv("AKAMAI_PASSWORD")
+var secret = os.Getenv("AKA_CCU_CLIENT_SECRET")
+var host = os.Getenv("AKA_CCU_HOST")
+var accessToken = os.Getenv("AKA_CCU_ACCESS_TOKEN")
+var clientToken = os.Getenv("AKA_CCU_CLIENT_TOKEN")
 var arl = os.Getenv("TEST_ARL")
 
-func TestPurge(t *testing.T) {
-	c, err := NewClient(user, pass, nil)
+func TestDelete(t *testing.T) {
+	conf := edgegrid.Config{
+		Host:         host,
+		AccessToken:  accessToken,
+		ClientSecret: secret,
+		ClientToken:  clientToken,
+		MaxBody:      50000,
+		Debug:        false,
+	}
+	c, err := NewClient(Staging, conf)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
 
 	ctx := context.Background()
-	res, err := c.Purge(ctx, arl)
+	res, err := c.Delete(ctx, arl)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
@@ -27,40 +40,36 @@ func TestPurge(t *testing.T) {
 	}
 }
 
-func TestGetQueueLength(t *testing.T) {
-	c, err := NewClient(user, pass, nil)
+func TestDeleteARLS(t *testing.T) {
+	t.Skip("local test only")
+
+	conf := edgegrid.Config{
+		Host:         host,
+		AccessToken:  accessToken,
+		ClientSecret: secret,
+		ClientToken:  clientToken,
+		MaxBody:      50000,
+		Debug:        false,
+	}
+	c, err := NewClient(Staging, conf)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
+
+	arls := make([]string, 0)
+	fp, err := os.Open("./testdata/500_arls.txt")
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	scan := bufio.NewScanner(fp)
+	for scan.Scan() {
+		arls = append(arls, scan.Text())
+	}
+	defer fp.Close()
 
 	ctx := context.Background()
-	res, err := c.GetQueueLength(ctx)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	if res == nil {
-		t.Fatalf("unexpected error: response struct is empty")
-	}
-}
-
-func TestGetPurgeStatus(t *testing.T) {
-	c, err := NewClient(user, pass, nil)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	ctx := context.Background()
-	r, err := c.Purge(ctx, arl)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	if r == nil {
-		t.Fatalf("unexpected error: response struct is empty")
-	}
-
-	res, err := c.GetPurgeStatus(ctx, r.ProgressUri)
+	res, err := c.Delete(ctx, arls...)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package restclient
 
-const version = "0.2.0"
+const version = "1.0.0"
